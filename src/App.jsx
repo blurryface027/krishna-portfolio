8d91620c7322166b46607b7eae8a1164df4c8103
blurryface027 <krishna150487@gmail.com>
2026-05-29 14:39:29 +0530

    Update favicon and remove Experience section

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
-import Experience from './components/Experience';
+// import Experience from './components/Experience';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
@@ -51,7 +51,7 @@ function App() {
       <About />
       <Skills />
       <Projects />
-      <Experience />
+      {/* <Experience /> */}
       <Contact />
       <Footer />
     </>
